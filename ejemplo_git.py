2b87f374cf85cfcc91bc67a7999e19144922cfc2
fernando_castillo <fern.castillop@duocuc.cl>
2024-05-29 09:22:39 -0400

    cree mi diccionario del ejercicio

---
+++ ejemplo_git.py
@@ -0,0 +1,8 @@
+#HOLA
+diccionario = {
+    "identificador":1,
+    "nombre": "Marriot",
+    "direccion": "Avenida Bernardo O'higgins 1123",
+    "funcionando": True,
+    "valor_habitacion":10000
+}
